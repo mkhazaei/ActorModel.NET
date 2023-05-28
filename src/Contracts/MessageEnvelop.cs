2d80487eb93af6db4864e3bf26d19f1aba342b17
Mohammad Khazaei <khazaei.m7@gmail.com>
2023-05-28 19:10:03 +0100

    add message envelop

---
+++ src/Contracts/MessageEnvelop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ActorModelNet.Contracts
+{
+    /// <summary>
+    /// Message Envelop to keep message and sender
+    /// </summary>
+    public class MessageEnvelop
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public MessageEnvelop(object message, IActorIdentity? sender)
+        {
+            Message = message;
+            Sender = sender;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public object Message { get; init; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IActorIdentity? Sender {  get; init; }
+
+
+    }
+}
