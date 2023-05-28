5a2bb305f4e38f96f8340c6980cfd7c278cf2823
Mohammad Khazaei <khazaei.m7@gmail.com>
2023-05-28 19:19:46 +0100

    add GuidActorIdentity

---
+++ src/System/GuidActorIdentity.cs
@@ -0,0 +1,50 @@
+﻿using ActorModelNet.Contracts;
+using System;
+
+namespace ActorModelNet.System
+{
+    /// <summary>
+    /// Long Account Identity
+    /// </summary>
+    public record GuidActorIdentity : IActorIdentity
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public GuidActorIdentity(Guid identity)
+        {
+            Identity = identity;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid Identity { get; }
+
+
+        
+
+        bool IEquatable<IActorIdentity>.Equals(IActorIdentity? other)
+        {
+            if (other == null) return false;
+            var castedOther = other as GuidActorIdentity;
+            if (castedOther == null) return false;
+            return castedOther.Identity == Identity;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override int GetHashCode() => Identity.GetHashCode();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public byte[] ToByeArray() => Identity.ToByteArray();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override string ToString() => Identity.ToString("N");
+    }
+}
