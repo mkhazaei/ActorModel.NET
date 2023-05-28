0a06c62f88d7a9e46c08c1aab787b7e533ccb6e1
Mohammad Khazaei <khazaei.m7@gmail.com>
2023-05-28 22:20:52 +0100

    implement ActorExecuter by IThreadPoolWorkItem

--- src/System/ActorExecuter.cs
+++ src/System/ActorExecuter.cs
@@ -14,7 +14,7 @@ namespace ActorModelNet.System
     /// There is no parent-child hierarchies and Clustering configuration.
     /// Actors will be deactivated and stored in DB and restored on demands.
     /// </summary>
-    public class ActorExecuter<TState> : IDisposable
+    public class ActorExecuter<TState> : IDisposable, IThreadPoolWorkItem
         where TState : class, IEquatable<TState>
     {
 
@@ -27,6 +27,7 @@ namespace ActorModelNet.System
         private readonly ILogger _logger;
 
         private TState _state; // Isolated State
+        private int _processStatus;
 
         #endregion
 
@@ -43,6 +44,7 @@ namespace ActorModelNet.System
             _identity = identity;
             _behaviourFactory = actor.Behaviour;
             _state = initState ?? actor.InitialState();
+            _processStatus = ActorStatus.Idle;
             _messageQueue = new ConcurrentQueue<(object Message, IActorIdentity? Sender)>();
             _logger = logger;
         }
@@ -63,7 +65,9 @@ namespace ActorModelNet.System
         /// </summary>
         public void Send(object message, IActorIdentity? sender = null)
         {
-            throw new NotImplementedException();
+            if (_processStatus == ActorStatus.Stopped) throw new ActorAlreadyStoppedException();
+            _messageQueue.Enqueue(new (message, sender));
+            ProcessQueues();
         }
 
 
@@ -72,9 +76,63 @@ namespace ActorModelNet.System
         /// </summary>
         public Task<TState> GetState()
         {
-            throw new NotImplementedException();
+            if (_processStatus == ActorStatus.Stopped) throw new ActorAlreadyStoppedException();
+            return Task.FromResult(_state); // FIXME: Unsafe
         }
 
+        /// <summary>
+        /// IDisposable.Dispose
+        /// </summary>
+        public void Dispose()
+        {
+            _processStatus = ActorStatus.Stopped;
+        }
+
+        #endregion
+
+        #region IThreadPoolWorkItem
+
+        /// <summary>
+        /// IThreadPoolWorkItem.Execute - Executed in new thread and handle Actor messages
+        /// </summary>
+        public void Execute()
+        {
+            bool anyModified = false;
+            for (int i = 0; i < _batchExecuteSize; i++)
+            {
+                if (_processStatus == ActorStatus.Stopped) break;
+                if (!_messageQueue.TryDequeue(out var envelop)) break;
+
+                var newState = _behaviourFactory().Handle(new MessageEnvelop(envelop.Message, envelop.Sender), _state);
+                var modified = !_state.Equals(newState);
+                if (modified)
+                {
+                    _state = newState;
+                    anyModified = anyModified || modified;
+                }
+            }
+
+            // Continue?
+            if (_processStatus == ActorStatus.Stopped)
+            {
+                return;
+            }
+            _processStatus = ActorStatus.Idle;
+            if (_messageQueue.Any()) ProcessQueues();
+        }
+
+        #endregion
+
+        #region Private Functions
+
+        private void ProcessQueues()
+        {
+            // If _status == Idle => _status = Occupied; return Idle (init value)
+            if (Interlocked.CompareExchange(ref _processStatus, ActorStatus.Occupied, ActorStatus.Idle) == ActorStatus.Idle)
+            {
+                ThreadPool.UnsafeQueueUserWorkItem(this, false);
+            }
+        }
 
         #endregion
 
---
+++ src/System/ActorStatus.cs
@@ -0,0 +1,24 @@
+﻿namespace ActorModelNet.System
+{
+    /// <summary>
+    /// Actor Life-Cycle
+    /// </summary>
+    public static class ActorStatus
+    {
+        /// <summary>
+        /// Idle
+        /// </summary>
+        public const int Idle = 1;
+
+        /// <summary>
+        /// Actor is Processing on Background Thread
+        /// </summary>
+        public const int Occupied = 2;
+
+        /// <summary>
+        /// Actor is Stopping
+        /// </summary>
+        public const int Stopped = 3;
+
+    }
+}
